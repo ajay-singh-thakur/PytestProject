c3a58922bfbd72e8de9d4f5107da544d0232ac20
ajay-singh-thakur <70859867+ajay-singh-thakur@users.noreply.github.com>
2020-09-09 01:37:19 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,13 +1,13 @@
-python -m pytest -s -v -m "sanity or regression" --html=./Reports/Report2.html testCases/ --browser chrome
+rem python -m pytest -s -v -m "sanity or regression" --html=./Reports/Report2.html testCases/ --browser chrome
 rem pytest -s -v --html=Reports\Report.html testCases/ --browser chrome
 rem pytest -s -v -m "sanity and regression" --html=./Reports/ReportChrome.html testCases/ --browser chrome
 rem pytest -s -v -m "sanity or regression" --html=./Reports/ReportChrome.html testCases/ --browser chrome
-rem pytest -s -v -m "regression" --html=./Reports/ReportChrome.html testCases/ --browser chrome
+python -m pytest -s -v -m "sanity or regression" --html=./Reports/ReportChrome.html testCases/test_login.py --browser chrome
 
 
-rem pytest -s -v -m "ajay" --html=./Reports/ReportFirefox.html testCases/ --browser firefox
+rem python -m pytest -s -v -m "sanity" --html=./Reports/ReportFirefox.html testCases/ --browser firefox
 rem pytest -s -v --html=Reports\Report.html testCases/ --browser firefox
 rem pytest -s -v -m "sanity and regression" --html=./Reports/ReportFirefox.html testCases/ --browser firefox
 rem pytest -s -v -m "sanity or regression" --html=./Reports/ReportFirefox.html testCases/ --browser firefox
-rem pytest -s -v -m "regression" --html=./Reports/ReportFirefox.html testCases/ --browser firefox
+python -m pytest -s -v -m "regression" --html=./Reports/ReportFirefox.html testCases/ --browser firefox
 
