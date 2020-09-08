2fc77f79211afefdae731e68a43b3fc46f1eabb8
ajay-singh-thakur <70859867+ajay-singh-thakur@users.noreply.github.com>
2020-09-09 01:16:37 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
-python -m pytest -s -v -m "sanity" --html=./Reports/Report2.html testCases/ --browser chrome
+rem python -m pytest -s -v -m "sanity" --html=./Reports/Report2.html testCases/ --browser chrome
 rem pytest -s -v --html=Reports\Report.html testCases/ --browser chrome
-rem pytest -s -v -m "sanity and regression" --html=./Reports/ReportChrome.html testCases/ --browser chrome
+pytest -s -v -m "sanity and regression" --html=./Reports/ReportChrome.html testCases/ --browser chrome
 rem pytest -s -v -m "sanity or regression" --html=./Reports/ReportChrome.html testCases/ --browser chrome
 rem pytest -s -v -m "regression" --html=./Reports/ReportChrome.html testCases/ --browser chrome
 
@@ -10,4 +10,4 @@ rem pytest -s -v --html=Reports\Report.html testCases/ --browser firefox
 rem pytest -s -v -m "sanity and regression" --html=./Reports/ReportFirefox.html testCases/ --browser firefox
 rem pytest -s -v -m "sanity or regression" --html=./Reports/ReportFirefox.html testCases/ --browser firefox
 rem pytest -s -v -m "regression" --html=./Reports/ReportFirefox.html testCases/ --browser firefox
-pause
+
